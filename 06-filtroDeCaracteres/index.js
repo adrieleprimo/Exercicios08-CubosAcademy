aaad80e10f2ee7658977a8fcec4f607a929daea0
Adriele Primo <adriele.dejprimo@gmail.com>
2023-09-06 13:52:55 -0300

    exercicio sobre cidades realizado

---
+++ 06-filtroDeCaracteres/index.js
@@ -0,0 +1,15 @@
+const cidades = [
+    "Salvador",
+    "São Paulo",
+    "Brasilia",
+    "Recife",
+    "Rio de Janeiro",
+];
+
+function validacaoCidades (cidades){
+    const validacao = cidades.filter((cidade)=>{
+        return cidade.length <= 8;
+    })
+    console.log(validacao.join(", "))
+}
+validacaoCidades(cidades);
